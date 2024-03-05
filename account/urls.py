de9fa72e93ae1236582e3ee6ccc2edb960961f7b
nurxon <nafasovnurxon18@gmail.com>
2024-03-05 18:59:01 +0500

    modified

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from rest_framework.authtoken import views as token_auth_view
 
+from .views import logout
+
 
 app_name = 'account'
 
 urlpatterns = [
     path('login/', token_auth_view.obtain_auth_token, name='login'),
+    path('logout/', logout, name='logout')
 ]
 
--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    user = request.user
+    Token.objects.filter(user=user).delete()
+    data = {
+        'success': True,
+        'message': 'Logged out successfully'
+    }
+    return Response(data, status=status.HTTP_200_OK)
+
